d9ad320889dc8c2e4321b3adc515de84ec9c37d7
Ari Souza <email.arisantos@gmail.com>
2023-09-20 20:16:21 -0300

    pratica 1 completa totalmente

--- pratica1B.m
+++ pratica1B.m
@@ -11,7 +11,7 @@ im = imread('C:\Users\arisa\OneDrive\Documentos\pdi\imgs\lena.png');
 # Mostrar Tamanho da Imagem no Console:
 # Exiba o tamanho da imagem “im” no console.
 size(im);
-figure
+figure('name', "Imagem Original");
 imshow(im);
 # Percorra a imagem “im” e adicione 80 às intensidades de cada pixel.
 # Verifique se ocorreu estouro de branco nos pixels.
@@ -21,23 +21,31 @@ for i=1:size(im2,1)
     im2(i,j) = im2(i,j) + 80;
   endfor
 endfor
-figure;
+figure('name', "Imagem com + 80");
 imshow(im2); %Houve estouro
 
 %Percorra a imagem resultante e subtraia 80 de cada pixel.
 %Observe se o estouro de branco foi removido.
-im3 = im;
-for i=1:size(im3,1)
-  for j=1:size(im3,2)
-    im3(i,j) = im3(i,j) - 80;
+imSubtraida = im;
+for i=1:size(imSubtraida,1)
+  for j=1:size(imSubtraida,2)
+    imSubtraida(i,j) = imSubtraida(i,j) - 80;
   endfor
 endfor
-figure;
-imshow(im3); % Estouro não foi resolvido
+figure('name', "Imagem com - 80");
+imshow(imSubtraida); % Resposta: Estouro não foi resolvido
+# Peça ao usuário para inserir um valor inteiro .
+#Repita o processo do ponto 4 com o valor inserido, gerando a imagem im3.
 
-
-
-
+usuario = input("Digite um numero inteiro: ");
+im3 = im;
+for i=1:size(im,1)
+  for j=1:size(im,2)
+    im3(i,j) = im3(i,j) + usuario;
+  endfor
+endfor
+figure('name', "Imagem do usuario");
+imshow(im3);
 
 
 
