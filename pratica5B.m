3ed566c4123943366b71743b7635a79bcd4d63f6
Ari Souza <email.arisantos@gmail.com>
2023-11-26 15:56:38 -0300

    pratice 5B

--- pratica5B.m
+++ pratica5B.m
@@ -2,18 +2,17 @@ close all;
 clear all;
 #pkg load image;
 #-----------------------------------
-# PRATICA 5
+# PRATICA 5B
 #-----------------------------------
 im = imread('milho.png');
 #figure('name', "Imagem 1");
 #imshow(im(:,:,3));
 imMask = zeros(size(im,1),size(im,2),'uint8');
-#imMask(:,:) = 255;
 
 for i=1: size(im,1)
   for j=1: size(im,2)
     if im(i,j,1) > 150
-      imMask(i,j) = 255;
+      imMask(i,j) = 255; #PINTAR DE BRANCO ONDE FOR MILHO NA MASCARA
     endif
   endfor
 endfor
